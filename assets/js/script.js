19a4888139ecfdcb5607d4ce2b202b09ee567472
Lomudru <kmejasson@gmail.com>
2023-05-15 11:29:03 +0200

    Adding a start of the way list and the number of person list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 let accomodation = document.getElementById("accomodation");
 let accomodationDot = document.getElementById("accomodation-dot");
+let selectWay = document.getElementById("select");
+let selectOption = document.getElementById("select-option");
+let person = document.getElementById("person");
+let optionPerson = document.getElementById("option-person");
 
 accomodation.addEventListener("click",function(){
     accomodationDot.classList.toggle("active");
     accomodationDot.classList.toggle("disabled");
+})
+
+selectWay.addEventListener("click",function () {
+    selectOption.classList.toggle("show");
+    selectOption.classList.toggle("hidden");
+})
+
+person.addEventListener("click", function () {
+    optionPerson.classList.toggle("show");
+    optionPerson.classList.toggle("hidden");
 })
