e6225798a2f7f291e091c2096229bf7eba6fabcd
Lomudru <kmejasson@gmail.com>
2023-05-17 16:29:51 +0200

    Working on the number of person

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,12 @@ let options = document.querySelectorAll("#select-option p");
 let optionSelected = document.getElementById("selected-option");
 let FirstCity = document.getElementById("FirstCity");
 let LastCity = document.getElementById("LastCity");
-let WayClicked = false;
-let canHideWay = false;
-let PersonClicked = false;
-let canHidePerson = false;
-
+let ChangingNumberPerson = document.querySelectorAll(".changing-number");
+let YouthAge = document.getElementById("YouthAge");
+let NumberPerson = document.querySelector("#person p");
+let Adult = document.getElementById("number-adult");
+let Youth = document.getElementById("number-youth");
+let Senior = document.getElementById("number-senior");
 accomodation.addEventListener("click",function(){
     accomodationDot.classList.toggle("active");
     accomodationDot.classList.toggle("disabled");
@@ -21,10 +22,6 @@ accomodation.addEventListener("click",function(){
 selectWay.addEventListener("click",function () {
     selectOption.classList.toggle("show");
     selectOption.classList.toggle("hidden");
-    WayClicked = true;
-    setTimeout(function () {
-        canHideWay = true;
-    }, 1);
     for (let j = 0; j < options.length; j++){
         if (options[j].innerHTML === optionSelected.innerHTML){
             options[j].parentElement.classList.add("actual");
@@ -38,10 +35,6 @@ selectWay.addEventListener("click",function () {
 person.addEventListener("click", function () {
     optionPerson.classList.toggle("show");
     optionPerson.classList.toggle("hidden");
-    PersonClicked = true;
-    setTimeout(function () {
-        canHidePerson = true;
-    }, 1);
 })
 
 for (let i = 0; i < options.length; i++){
@@ -52,7 +45,75 @@ for (let i = 0; i < options.length; i++){
 document.addEventListener("click", function (){
     FirstCity.innerHTML = "";
     LastCity.innerHTML = "";
-
 })
 
 
+for (let k = 0; k < ChangingNumberPerson.length; k++){
+    ChangingNumberPerson[k].addEventListener("click", function () {
+        optionPerson.classList.toggle("show");
+        optionPerson.classList.toggle("hidden");
+        if (ChangingNumberPerson[k].id === "less-adult" || ChangingNumberPerson[k].id === "more-adult"){
+                if (ChangingNumberPerson[k].id === "less-adult"){
+                    if (parseInt(Adult.innerText) > 0){
+                        Adult.innerText = parseInt(Adult.innerText) - 1;
+                        if (parseInt(Adult.innerText) === 0){
+                            ChangingNumberPerson[k].parentNode.classList.add("disabledAddPerson");
+                        }
+                    }
+                }
+                else if (ChangingNumberPerson[k].id === "more-adult"){
+                    Adult.innerText = parseInt(Adult.innerText) + 1;
+                    ChangingNumberPerson[k].parentNode.classList.remove("disabledAddPerson");
+                }
+        }
+        else if (ChangingNumberPerson[k].id === "less-youth" || ChangingNumberPerson[k].id === "more-youth"){
+                if (ChangingNumberPerson[k].id === "less-youth"){
+                    if (parseInt(Youth.innerText) > 0){
+                        Youth.innerText = parseInt(Youth.innerText) - 1;
+                        if (parseInt(Youth.innerText) === 0){
+                            ChangingNumberPerson[k].parentNode.classList.add("disabledAddPerson");
+                            YouthAge.innerHTML = "";
+                        }
+                    }
+                }
+                else if (ChangingNumberPerson[k].id === "more-youth"){
+                    Youth.innerText = parseInt(Youth.innerText) + 1;
+                    ChangingNumberPerson[k].parentNode.classList.remove("disabledAddPerson");
+                    YouthAge.innerHTML += "<p>Youth "+ Youth.innerText +"</p>" +
+                        "<select id='YouthSelect"+ Youth.innerText +"'><option value='' disabled selected hidden>Age</option></select>";
+                    for (let m = 0; m < 25; m++){
+                        YouthAge.querySelector("YouthSelect" + YouthAge.innerHTML +"").innerHTML += "<option>"+ m +" years</option>"
+                    }
+                }
+        }
+        else if (ChangingNumberPerson[k].id === "less-senior" || ChangingNumberPerson[k].id === "more-senior"){
+                if (ChangingNumberPerson[k].id === "less-senior"){
+                    if (parseInt(Senior.innerText) > 0){
+                        Senior.innerText = parseInt(Senior.innerText) - 1;
+                        if (parseInt(Senior.innerText) === 0){
+                            ChangingNumberPerson[k].parentNode.classList.add("disabledAddPerson");
+                        }
+                    }
+                }
+                else if (ChangingNumberPerson[k].id === "more-senior"){
+                    Senior.innerText = parseInt(Senior.innerText) + 1;
+                    ChangingNumberPerson[k].parentNode.classList.remove("disabledAddPerson");
+                }
+        }
+        if (parseInt(Youth.innerText) > 0 || parseInt(Senior.innerText) > 0){
+            NumberPerson.innerText = parseInt(Adult.innerText) + parseInt(Youth.innerText) + parseInt(Senior.innerText) + " Passengers, No discound card"
+        }
+        else {
+            if (parseInt(Adult.innerText) + parseInt(Youth.innerText) + parseInt(Senior.innerText) > 1){
+                NumberPerson.innerText = parseInt(Adult.innerText) + parseInt(Youth.innerText) + parseInt(Senior.innerText) + " Adults, No discound card"
+            }
+            else {
+                NumberPerson.innerText = parseInt(Adult.innerText) + parseInt(Youth.innerText) + parseInt(Senior.innerText) + " Adult, No discound card"
+            }
+        }
+    })
+}
+YouthAge.addEventListener("click", function () {
+    optionPerson.classList.toggle("show");
+    optionPerson.classList.toggle("hidden");
+})
